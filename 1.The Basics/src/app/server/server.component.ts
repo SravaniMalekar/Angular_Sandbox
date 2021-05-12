8804e4491530c7768087bde3b1de0d667a799b2c
SRAVANI SAI M <SRAVANI.SAI.M@ibm.com>
2021-05-12 17:39:47 +0530

    basics

---
+++ 1.The Basics/src/app/server/server.component.ts
@@ -0,0 +1,28 @@
+//New Component
+
+import { Component } from "@angular/core";
+//component declarator
+@Component({
+    selector : 'app-server',
+    templateUrl : './server.component.html',
+    styles:[ `
+    .online{
+        color: white;
+    }`]
+})
+export class ServerComponent{
+    serverID: number = 10;
+    serverStatus: string = 'offline';
+
+    constructor(){
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+    }
+
+    getServerStatus(){
+        return this.serverStatus;
+    }
+
+    getColor(){
+        return this.serverStatus === 'online' ? 'green' : 'red';
+    }
+}
